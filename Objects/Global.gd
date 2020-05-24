ee3ef8e949b72858b766772667b3f7d931fd5b84
Ihor Fox <Ihor.fox@gmail.com>
2020-05-24 16:36:41 +0300

    Score had been fixed

--- Objects/Global.gd
+++ Objects/Global.gd
@@ -1,3 +1,19 @@
-extends Node2D
+extends Node
 
 var score = 0
+
+signal score_updated(n)
+
+func score_set(n):
+	score = n
+	if n < 0: n = 0
+	score_update()
+
+func score_get():
+	return score
+
+func score_add(n):
+	score_set(score_get() + n)
+
+func score_update():
+	emit_signal("score_updated", score)
